240d37ae5a7dea567244a1c72193829c10b728eb
rocket-builder <bogdan14.228@gmail.com>
2020-07-06 23:37:07 +0300

    change letter

--- php/controllers/region-get-controller.php
+++ php/controllers/region-get-controller.php
@@ -5,6 +5,6 @@ require '../models/response.php';
 if (isset($_POST[])) {
   $regions = R::findAll('region');
 
-  echo json_encode(new Response("success", false, $regions));
+  echo json_encode(new Response("Success", false, $regions));
 }
 ?>
