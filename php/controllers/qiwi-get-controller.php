7cb24f410a8d97ececf8bb2ef72a69bb0a70fed3
rocket-builder <bogdan14.228@gmail.com>
2020-07-21 03:34:18 +0300

    Update qiwi-get-controller.php

--- php/controllers/qiwi-get-controller.php
+++ php/controllers/qiwi-get-controller.php
@@ -2,7 +2,7 @@
 require '../models/response.php';
 require '../connection.php';
 
-$qiwi = R::findAll( 'qiwi' , ' ORDER BY id DESC LIMIT 1');
+$qiwi = R::findOne( 'qiwi' , ' ORDER BY id DESC LIMIT 1');
 
 echo json_encode(new Response("Success :)", false, [ 'qiwi' => $qiwi ]));
 ?>
